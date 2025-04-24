65704fdbf48204a1f5e3abf4beae705a20bf6640
flsp <florian.lesp@gmail.com>
2025-04-24 09:00:47 +0200

    feat: export all liked titles

--- export_spotify.py
+++ export_spotify.py
@@ -14,3 +14,19 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
     scope="user-library-read playlist-read-private"
 ))
+
+# export des titres likés
+tracks = []
+results = sp.current_user_saved_tracks()
+while  results:
+    for item in results['items']:
+        track = item['track']
+        tracks.append([track["name"], track['artists'][0]['name']])
+    if results['next']:
+        results = sp.next(results)
+    else:
+        break
+
+# sauvegarde des titres dans un fichier csv
+df = pandas.DataFrame(tracks, columns=["Titre", "Artiste"])
+df.to_csv("titres_likes.csv", index=False)
